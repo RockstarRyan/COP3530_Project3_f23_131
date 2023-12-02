d180ec48231883e36b16bdb8ff8546b99d17e085
Jackson Kelly <116661139+JacksonKelly2354@users.noreply.github.com>
2023-12-02 15:29:05 -0500

    test for graph

---
+++ graph/test.py
@@ -0,0 +1,8 @@
+from graph import graph
+if __name__ == "__main__":
+    network = graph()
+    for i in range(10):
+        network.generateIP()
+    for i in network.ips:
+        network.connect(i)
+    network.traverse()
