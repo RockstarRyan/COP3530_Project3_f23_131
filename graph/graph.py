8e7493f5298221613b8b143e399d4e4b86207d81
Jackson Kelly <116661139+JacksonKelly2354@users.noreply.github.com>
2023-12-02 14:25:01 -0500

    Random generation of data values, currently has issues with parallel edges
    Added function that randomly generates IP address and a function that randomly generates connections for a given IP address.

--- graph/graph.py
+++ graph/graph.py
@@ -1,3 +1,4 @@
+import random
 class nodeInfo:
     def __init__(self, destinationIp : str, speed : float, latency : float, bandwidth : float):
         self.destinationIp = destinationIp
@@ -8,6 +9,7 @@ class nodeInfo:
 class graph:
     def __init__(self):
         self.graph = dict()
+        self.ips = set()
     def insert(self, sourceIp : str, destinationIp : str, speed : float, latency : float, bandwidth : float):
         try:
             sourceIp = str(sourceIp)
@@ -28,3 +30,30 @@ class graph:
             print(i, end=": ")
             for j in index:
                 print(j.destinationIp, j.speed, j.latency, j.bandwidth)
+    def generateIP(self):
+        ip = ""
+        for i in range(4):
+            num = random.randint(0, 255)
+            ip += str(num)
+            if i != 3:
+                ip += "."
+        self.ips.add(ip)
+    def connect(self, _sourceIP : str):
+        # randomly generates number of connections
+        numConnect = random.randint(0, len(self.ips))
+        connections = set()
+        # makes sure connection hasn't already been made
+        if self.graph.get(_sourceIP):
+            for j in self.graph[_sourceIP]:
+                connections.add(j)
+        for i in range(numConnect):
+            # picks destination, checks that it isn't source or existing connection
+            dest = random.choice(tuple(self.ips))
+            if dest != _sourceIP and dest not in connections:
+                _speed = float(random.randint(200, 2000)) / 10
+                _latency = float(random.randint(100, 400)) / 10
+                _bandwidth = float(random.randint(30, 300)) / 10
+
+                self.insert(_sourceIP, dest, _speed, _latency, _bandwidth)
+                self.insert(dest, _sourceIP, _speed, _latency, _bandwidth)
+                connections.add(dest)
