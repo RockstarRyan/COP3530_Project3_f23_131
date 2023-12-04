08fb6e84cb63c0685f48b6ecb22dc1f475ef93e7
RockstarRyan <ryan@rockstarryan.net>
2023-12-03 22:16:41 -0500

    Update output.py

--- graph/output.py
+++ graph/output.py
@@ -5,13 +5,16 @@ def main():
     # Don't worry about these for now...
     router1 = "1.1.1.1"
     router2 = "2.2.2.2"
-
-    # Call graph algorithms
-    resultsB = algorithms.bellmanFord(router1,router2)
-    resultsD = algorithms.dijkstras(router1,router2)
-    
-    # Print algorithm results in JSON format (to be read by JS program)
-    print("{bellmanFord:{speed:"+resultsB.speed+", latency:"+resultsB.latency+", bandwidth:"+resultsB.bandwidth+"}, dijkstras:{speed:"+resultsD.speed+", latency:"+resultsD.latency+", bandwidth:"+resultsD.bandwidth+"}")
+    if router1 == router2:
+        print("{error:'"+"Router addresses cannot be the same!"+"'}")
+        
+    else:
+        # Call graph algorithms
+        resultsB = algorithms.bellmanFord(router1,router2)
+        resultsD = algorithms.dijkstras(router1,router2)
+        
+        # Print algorithm results in JSON format (to be read by JS program)
+        print("{bellmanFord:{speed:'"+resultsB.speed+"', latency:'"+resultsB.latency+"', bandwidth:'"+resultsB.bandwidth+"'}, dijkstras:{speed:'"+resultsD.speed+"', latency:'"+resultsD.latency+"', bandwidth:'"+resultsD.bandwidth+"'}")
 
 if __name__ == '__main__':
     main()
